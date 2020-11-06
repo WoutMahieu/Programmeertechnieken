9298e71567f263608db8fcb2f6c59ff398e9bdec
WoutMahieu <wout.mahieu@student.kuleuven.be>
2020-11-06 10:40:52 +0100

    completed Locked & Forced states

--- Programmeertechnieken/src/FSM.c
+++ Programmeertechnieken/src/FSM.c
@@ -29,21 +29,46 @@ void FSM_UpdateStates(void){
 			FSM_currentState = Forced;
 			FSM_EnterForced();
 		}
+
 		break;
 	case Opened:
 		FSM_Opened();
 
 		//Check for valid RFID: if valid => Locked else check for joystick center => Config
+		if(0){
+			FSM_ExitOpened();
+			FSM_CurrentState = Locked;
+			FSM_EnterLocked();
+		}
+		else if(0){
+			FSM_ExitOpened();
+			FSM_CurrentState = Config;
+			FSM_EnterConfig();
+		}
+
 		break;
 	case Forced:
 		FSM_Forced();
 
 		//Check if valid tag scanned, if !valid => nothing, else check contactswitch, if '1' => Locked, else => Opened
+		if(0){
+			if(ContactSwitch_GetFlag() == 1){
+				FSM_ExitForced();
+				FSM_CurrentState = Locked;
+				FSM_EnterLocked();
+			}
+			else{
+				FSM_ExitForced();
+				FSM_CurrentState = Opened;
+				FSM_EnterOpened();
+			}
+		}
+
 		break;
 	case Config:
 		FSM_Config();
 
-		//Check for joystick center => Opened, else nothing
+		//Check for joystick center, if '1' => check contactswitch, if '1' => Locked, else => Opened
 		break;
 	}
 }
@@ -73,6 +98,7 @@ void FSM_ExitLocked(void){
 void FSM_EnterOpened(void){
 	//Display opened screen & unlock
 	DisplayControl_OpenedScreen();
+	OutputControl_Unlock();
 }
 
 void FSM_Opened(void){}
